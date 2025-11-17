a56c510e358fe9e9336bf17f201b3002fea15757
Slayer_1104 <69082260+nishanth1104@users.noreply.github.com>
2025-11-17 12:59:45 -0500

    Logo Relocating in ADMIN PAGE

--- frontend/app/admin/page.js
+++ frontend/app/admin/page.js
@@ -264,7 +264,7 @@ export default function AdminDashboard() {
               <img 
                 src="/neurobud-logo.png" 
                 alt="Neurobud Logo" 
-                className="w-12 h-12 mx-auto mb-0"
+                className="w-12 h-12"
               /> Neurobud
             </Link>
             <span className="text-gray-400">|</span>
